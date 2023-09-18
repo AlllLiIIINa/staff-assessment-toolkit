6aaa96eac3ac18f78801c054127ae6842fd74546
ALiNa <alina.tvvv@gmail.com>
2023-09-18 21:59:40 +0300

    Init project & create endpoint health check

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def health_check():
+    response_content = {
+        "status_code": 200,
+        "detail": "ok",
+        "result": "working"
+    }
+    return JSONResponse(content=response_content)
