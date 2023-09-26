9ebf9ae66f07813e7d1f1db7e052166c0aeef72e
ALiNa <alina.tvvv@gmail.com>
2023-09-26 13:11:08 +0300

    Use depends for check_postgresql & check_redis

--- app/db/db.py
+++ app/db/db.py
@@ -1,14 +1,17 @@
 from redis import asyncio
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.config import Settings
 
 
-async def get_redis_db() -> Redis:
-    redis = await asyncio.from_url(Settings.REDIS_URL)
-    return redis
+async def get_redis() -> Redis:
+    return await asyncio.from_url(Settings.REDIS_URL)
 
 
-async def get_postgresql_db():
-    engine = create_async_engine(Settings.DB_URL, echo=True, future=True)
-    return engine
+engine = create_async_engine(Settings.DB_URL, echo=True, future=True)
+
+
+async def get_db() -> AsyncSession:
+    async with AsyncSession(engine) as session:
+        return session
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,33 +11,3 @@ def test_health_check(client):
     assert "status_code" in data
     assert "detail" in data
     assert "result" in data
-
-
-def test_check_postgresql(client):
-    response = client.get("/check_postgresql")
-    assert response.status_code == 200
-    assert response.json() == {
-        "status_code": 200,
-        "detail": "PostgreSQL is healthy",
-        "result": "working"
-    }
-    data = response.json()
-
-    assert "status_code" in data
-    assert "detail" in data
-    assert "result" in data
-
-
-def test_check_redis(client):
-    response = client.get("/check_redis")
-    assert response.status_code == 200
-    assert response.json() == {
-        "status_code": 200,
-        "detail": "Redis is healthy",
-        "result": "working"
-    }
-    data = response.json()
-
-    assert "status_code" in data
-    assert "detail" in data
-    assert "result" in data
