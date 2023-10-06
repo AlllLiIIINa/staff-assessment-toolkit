23544c301c764f5c0fd4dbc1ed781fe7aa87edc1
ALiNa <alina.tvvv@gmail.com>
2023-10-06 19:09:12 +0300

    Add alembic.ini

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,8 @@ from app.db import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+section = config.config_ini_section
+config.set_section_option(section, "DATABASE_URL", Settings.DB_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
