27390ea48d4783ccce64f5cac70e6603570002a4
ALiNa <alina.tvvv@gmail.com>
2023-10-09 13:30:47 +0300

    Reduce code in user routers

--- app/routers/user_routers.py
+++ app/routers/user_routers.py
@@ -21,27 +21,23 @@ async def user_list(
 @router.post("/user_create", status_code=HTTPStatus.CREATED, operation_id="user_signup")
 async def user_create(user_data: SignUp, session: AsyncSession = Depends(get_db)):
     user_repo = UserService(session)
-    user = await user_repo.create(user_data)
-    return user
+    return await user_repo.create(user_data)
 
 
 @router.get("/user_get_by_id/{user_id}", response_model=SignIn, operation_id="user_signin")
 async def user_get_by_id(user_id: str, session: AsyncSession = Depends(get_db)):
     user_repo = UserService(session)
-    user = await user_repo.get_by_id(user_id)
-    return user
+    return await user_repo.get_by_id(user_id)
 
 
 @router.put("/user_update/{user_id}", response_model=UserBase, operation_id="user_update")
 async def user_update(user_id: str, user_data: UserUpdate, session: AsyncSession = Depends(get_db)):
     user_repo = UserService(session)
-    user = await user_repo.update(user_id, user_data)
     logging.info("Getting user processed successfully")
-    return user
+    return await user_repo.update(user_id, user_data)
 
 
 @router.put("/user_delete/{user_id}", status_code=HTTPStatus.NO_CONTENT, operation_id="user_delete")
 async def user_delete(user_id: str, session: AsyncSession = Depends(get_db)):
     user_repo = UserService(session)
-    user = await user_repo.delete(user_id)
-    return user
+    return await user_repo.delete(user_id)
