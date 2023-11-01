26dddc71d734761ef62ad052f8733eb3c89b5918
ALiNa <alina.tvvv@gmail.com>
2023-11-01 19:03:36 +0200

    Add Company table & CRUD for it

---
+++ app/routers/company_routers.py
@@ -0,0 +1,43 @@
+from http import HTTPStatus
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.services.companies import CompanyService
+from ..db.db import get_db
+from ..db.models import User
+from ..depends.depends import get_current_user
+from ..schemas.company import CompanyUpdate, CompanyBase
+
+company_router = APIRouter()
+
+
+@company_router.get("/companies/", operation_id="companies",)
+async def company_list(
+        page: int = Query(default=1, description="Page number", ge=1),
+        companies_per_page: int = Query(default=10, description="Items per page", le=100),
+        session: AsyncSession = Depends(get_db)):
+    company_repo = CompanyService(session)
+    return await company_repo.get_all(page, companies_per_page)
+
+
+@company_router.post("/company_create", status_code=HTTPStatus.CREATED, operation_id="company_create")
+async def company_create(company_data: CompanyBase, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_db)):
+    company_repo = CompanyService(session)
+    return await company_repo.create(company_data, user.user_id)
+
+
+@company_router.get("/company_get_by_id/{company_id}", operation_id="company_get_by_id")
+async def company_get_by_id(company_id: str, session: AsyncSession = Depends(get_db)):
+    company_repo = CompanyService(session)
+    return await company_repo.get_by_id(company_id)
+
+
+@company_router.put("/company_update/{company_id}", response_model=CompanyUpdate, operation_id="company_update")
+async def company_update(company_id: str, company_data: CompanyUpdate, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_db)):
+    company_repo = CompanyService(session)
+    return await company_repo.update(company_id, company_data, user.user_id)
+
+
+@company_router.put("/company_delete/{company_id}", status_code=HTTPStatus.NO_CONTENT, operation_id="company_delete")
+async def company_delete(company_id: str, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_db)):
+    company_repo = CompanyService(session)
+    return await company_repo.delete(company_id, user.user_id)
---
+++ app/schemas/company.py
@@ -0,0 +1,62 @@
+import datetime
+from uuid import UUID, uuid4
+from typing import Optional
+from pydantic import BaseModel, Field, FilePath, HttpUrl, ConfigDict
+
+
+class CompanyBase(BaseModel):
+    company_id: UUID = Field(json_schema_extra={"primary_key": True, "unique": True}, default_factory=uuid4, title="ID")
+    company_name: Optional[str] = Field(None, title="Name")
+    company_title: Optional[str] = Field(None, title="Title")
+    company_description: Optional[str] = Field(None, title="Description")
+    company_city: Optional[str] = Field(None, title="City")
+    company_phone: Optional[str] = Field(None, title="Phone")
+    company_links: Optional[HttpUrl] = Field(None, title="Link")
+    company_avatar: Optional[FilePath] = Field(None, title="Avatar")
+    company_is_visible: bool = Field(False, title="Visible")
+    company_created_at: Optional[datetime.datetime] = Field(None, title="Created")
+    company_updated_at: Optional[datetime.datetime] = Field(None, title="Updated")
+    owner_id: Optional[UUID] = Field(default=None)
+
+    def __repr__(self):
+
+        return (
+            f"<{self.__class__.__name__}("
+            f"company_id={self.company_id}, "
+            f"company_name={self.company_name}, "
+            f"company_title={self.company_title}, "
+            f"company_description={self.company_description}, "
+            f"company_is_visible={self.company_is_visible}, "
+            f")>"
+        )
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example": {
+                "company_name": "Company Name",
+                "company_title": "Company Title",
+                "company_description": "Company Description",
+                "company_is_visible": "False",
+            }
+        }
+    )
+
+
+class CompanyUpdate(BaseModel):
+    company_name: Optional[str] = None
+    company_title: Optional[str] = None
+    company_description: Optional[str] = None
+    company_is_visible: Optional[bool] = None
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example": {
+                "company_name": "",
+                "company_title": "",
+                "company_description": "",
+                "company_is_visible": "",
+            }
+        }
+    )
